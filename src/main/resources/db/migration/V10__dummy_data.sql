aa9cc2c26160fb7b04502fdac7d4b7d59fd08e19
Cemre <cemredisli@hotmail.com>
2025-09-08 12:47:14 +0300

    database changed

--- src/main/resources/db/migration/V10__dummy_data.sql
+++ src/main/resources/db/migration/V10__dummy_data.sql
@@ -2,9 +2,12 @@
 INSERT INTO tags (name, created_by, updated_by) VALUES
 ('Java', 'system', 'system'),
 ('Spring Boot', 'system', 'system'),
-('Frontend', 'system', 'system'),
+('Python', 'system', 'system'),
+('JavaScript', 'system', 'system'),
 ('Cloud', 'system', 'system'),
 ('Data Science', 'system', 'system');
+('React', 'system', 'system');
+('Veri Görselleştirme', 'system', 'system');
 
 -- CATEGORIES
 INSERT INTO categories (name, created_by, updated_by) VALUES
@@ -13,6 +16,9 @@ INSERT INTO categories (name, created_by, updated_by) VALUES
 ('Veri Bilimi', 'system', 'system'),
 ('Bulut Bilişim', 'system', 'system'),
 ('Kariyer', 'system', 'system');
+('Backend', 'system', 'system');
+('Frontend', 'system', 'system');
+('Web Programlama', 'system', 'system')
 
 -- JOBS
 INSERT INTO jobs (title, description, created_at, updated_at, created_by, updated_by) VALUES
@@ -51,19 +57,27 @@ INSERT INTO career_path_courses (career_path_id, course_id, order_no) VALUES
 -- courses and tags
 INSERT INTO course_tags (course_id, tag_id) VALUES
 (1, 1), -- Java -> Java tag'i
-(1, 2), -- Java -> Spring Boot tag'i
+(2, 3), -- İleri Python -> Python tag'i
 (2, 5), -- İleri Python -> Data Science tag'i
-(3, 3), -- React -> Frontend tag'i
-(4, 5), -- Veri Manipülasyonu -> Data Science tag'i
+(2, 5), -- İleri Python -> Veri Görselleştirme tag'i
+(5, 1), -- İleri Java -> Java tag'i
+(3, 4), -- React -> JS tag'i
+(3, 7), -- React -> React tag'i
+(4, 6), -- Veri Manipülasyonu -> Data Science tag'i
+(4, 8), -- Veri Manipülasyonu -> Data Science tag'i
 (5, 1); -- İleri Java -> Java tag'i
+(5, 2); -- İleri Java -> Spring Boot tag'i
 
 -- blogs and tags
 INSERT INTO blog_tags (blog_id, tag_id) VALUES
-(1, 3), -- React.js Güvenli Yazılım Pratikleri -> Frontend tag'i
-(2, 1), -- Regex -> Java tag'i (veya uygun olan)
-(3, 3), -- Web Yazılımına Nereden Başlanmalı? -> Frontend tag'i
-(4, 3), -- Redux Kurulumu -> Frontend tag'i
-(5, 5); -- Pandas ile Veri Analizi -> Data Science tag'i
+(1, 7), -- React.js Güvenli Yazılım Pratikleri -> React tag'i
+(1, 4), -- React.js Güvenli Yazılım Pratikleri -> JS tag'i
+(2, 6), -- Regex -> Veri tag'i
+(3, 1), -- Web yazılımına Nereden Başlanmalı?-> Java tag'i
+(3, 2), -- Web yazılımına Nereden Başlanmalı?-> Java tag'i
+(3, 4), -- Web yazılımına Nereden Başlanmalı? -> JS tag'i
+(4, 7), -- Redux Kurulumu -> React tag'i
+(5, 6); -- Pandas ile Veri Analizi -> Data Science tag'i
 
 -- jobs and tags
 INSERT INTO job_tags (job_id, tag_id) VALUES
@@ -76,18 +90,24 @@ INSERT INTO job_tags (job_id, tag_id) VALUES
 
 -- courses and categories
 INSERT INTO course_categories (course_id, category_id) VALUES
-(1, 1), -- Java -> Yazılım Geliştirme
-(2, 3), -- İleri Python -> Veri Bilimi
-(3, 1), -- React -> Yazılım Geliştirme
-(4, 3), -- Veri Manipülasyonu -> Veri Bilimi
-(5, 1); -- İleri Java -> Yazılım Geliştirme
+(1, 1), -- Java -> yazılım geliştirme
+(1, 2), -- Java -> mobil uygulama
+(1, 6), -- Java -> backend
+(2, 3), -- İleri Python -> veri bilimi
+(2, 7), -- İleri Python -> frontend
+(3, 7), -- React -> Frontend
+(4, 3), -- Veri Manipülasyonu -> veri bilimi
+(5, 1); -- İleri Java -> yazılım geliştirme
+(5, 2); -- İleri Java -> mobil uygulama
+(5, 6); -- İleri Java -> backend
 
 -- blogs and categories
 INSERT INTO blog_categories (blog_id, category_id) VALUES
-(1, 1), -- React.js blogu -> Yazılım Geliştirme
+(1, 7), -- React.js blogu -> frontend
+(1, 8), -- React.js blogu -> Web programlama
 (2, 1), -- Regex blogu -> Yazılım Geliştirme
-(3, 1), -- Web Yazılımı blogu -> Yazılım Geliştirme
-(4, 1), -- Redux blogu -> Yazılım Geliştirme
+(3, 8), -- Web Yazılımı blogu -> web prog
+(4, 7), -- Redux blogu -> frontend
 (5, 3); -- Pandas blogu -> Veri Bilimi
 
 -- jobs and categories
